806d97531463d6d15fb776ac0f41c01013650338
Chocolayte <ludovic.baudin@utbm.fr>
2016-04-22 14:39:58 +0200

    Exit if NoelPush is ever executed on the PC.

--- NPush/Services/DownloadService.cs
+++ NPush/Services/DownloadService.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using System.Windows;
 
 using NoelPush.ViewModels;
 
@@ -18,14 +22,21 @@ namespace NoelPush.Services
 
         public static void Download(MainWindowViewModel mainWindowViewModel)
         {
+            var processlist = Process.GetProcesses();
+            if (processlist.Any(process => process.ProcessName.ToLower() == "noelpush"))
+            {
+                MessageBox.Show("NoelPush est déjà exécuté !");
+                Close();
+            }
+
             viewModel = mainWindowViewModel;
-            watch = new Stopwatch();
 
             var client = new WebClient();
             client.DownloadProgressChanged += InProgress;
             client.DownloadFileCompleted += Completed;
-
             client.DownloadFileAsync(new Uri("http://noelpush.com/noelpush.exe"), "noelpush.exe");
+
+            watch = new Stopwatch();
             watch.Start();
         }
 
@@ -36,7 +47,7 @@ namespace NoelPush.Services
             var time = (100 - e.ProgressPercentage) * (watch.ElapsedMilliseconds / 1000.0) / e.ProgressPercentage + 1;
             time = Math.Round(time, 0);
 
-            viewModel.RemainingTime = time.ToString();
+            viewModel.RemainingTime = time.ToString(CultureInfo.InvariantCulture);
         }
 
         private static void Completed(object sender, AsyncCompletedEventArgs e)
@@ -49,7 +60,7 @@ namespace NoelPush.Services
             timer = new Timer(Close, null, 1000, 1000);
         }
 
-        private static void Close(object state)
+        private static void Close(object state = null)
         {
             Environment.Exit(0);
         }
